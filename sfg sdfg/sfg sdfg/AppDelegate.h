84397e450afbe10417be8a70202539b5f1cae9ee
dheeraj Singh <dheeraj.singh@anktech.co.in>
2013-05-30 16:50:08 +0530

    sdfgsdfg sdfg sdfg

---
+++ sfg sdfg/sfg sdfg/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  sfg sdfg
+//
+//  Created by Anktech on 5/30/13.
+//  Copyright (c) 2013 Anktech Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ sfg sdfg/sfg sdfg/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  sfg sdfg
+//
+//  Created by Anktech on 5/30/13.
+//  Copyright (c) 2013 Anktech Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
